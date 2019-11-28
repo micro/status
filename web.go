f52abe44d491a3a0e5a99f03fe170729747cb4aa
Asim Aslam <asim@aslam.me>
2019-11-28 11:25:01 +0000

    switch to go-micro web app

--- web.go
+++ web.go
@@ -3,10 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/micro/go-micro/web"
 )
 
 func main() {
-	// Simple static webserver:
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
+	service := web.NewService(
+		web.Name("go.micro.web.status"),
+		web.Address(":8080"),
+		web.Handler(http.FileServer(http.Dir("."))),
+	)
+	// parse command line
+	service.Init()
+	// run the service
+	log.Fatal(service.Run())
 }
 
